18cd93337eef50128f8e771142e552a067b40bcf
John Widmaier <johnwid92@gmail.com>
2019-07-26 18:00:21 -0400

    Finished Basic API Functions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 from bson.json_util import dumps
 import database
 
@@ -15,15 +15,42 @@ def get_task(task_id):
 		abort(404)
 	return jsonify(task)
 
-# @app.route('/todo/api/v1.0/tasks', methods=['POST'])
-# def create_task():
-# 	if not request.json or not 'title' in request.json:
-# 		abort(400)
-# 	task = {
-# 		'_id' : # get highest id in db and add 1,
-# 		'title' : request.json['title'],
-# 	}
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+	if not request.json or not 'title' in request.json:
+		abort(400)
+	task = {
+		'_id' : database.tasks.count_documents({}) + 1,
+		'title' : request.json['title'],
+		'description': request.json.get('description', ""),
+		'done': False
+	}
+	database.tasks.insert_one(task)
+	return jsonify(task), 201
 
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+	task = database.db.tasks.find_one({'_id' : task_id})
+	if not task:
+		abort(404)
+	if not request.json:
+		abort(400)
+	if 'title' in request.json and type(request.json['title']) != str:
+		abort(400)
+	if 'description' in request.json and type(request.json['description']) != str:
+		abort(400)
+	if 'done' in request.json and type(request.json['done']) != bool:
+		abort(400)
+	database.tasks.update_one({'_id' : task_id}, { '$set' : request.json})
+	return request.json
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+	task = database.db.tasks.find_one({'_id' : task_id})
+	if not task:
+		abort(404)
+	database.tasks.delete_one({'_id' : task_id})
+	return jsonify({'result': True})
 
 @app.errorhandler(404)
 def not_found(error):
--- database.py
+++ database.py
@@ -3,3 +3,10 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client['todoDB']
 tasks = db['tasks']
+
+
+
+if __name__ == "__main__":
+    
+    print(db.tasks.find_one({'_id' : 3}))
+    # print(tasks.count_documents({}))
